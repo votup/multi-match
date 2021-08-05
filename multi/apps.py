63b5255e4f463a090babc7ebf7b6136cd870e377
milee yu <mileeyu@gmail.com>
2021-08-04 20:36:28 -0700

    first commit

---
+++ multi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MultiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'multi'
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Boulder Beat Quiz App
