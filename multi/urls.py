3276f24c4990052ffe84214a794f5d18482aa32e
sandra fish <sandrafish@comcast.net>
2021-08-07 16:40:05 -0600

    initial views, urls, templates

--- multi/urls.py
+++ multi/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from django.conf import settings
+from django.views.generic.detail import DetailView
+
 from . import views 
 
 app_name = 'multi'
 urlpatterns = [
-  path('', views.index, name='index')
+  path('', views.index, name='index'),
+  path('about/', views.about, name='about'),
+  path('candidate_detail/(P<slug>[-\w]+)', views.CandidateDetailView, name='candidate_detail'),
+    
+    
 ]
--- multi/views.py
+++ multi/views.py
@@ -1,4 +1,32 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+
+
+from .models import Candidate, Question, Response, Voter
 
 def index(request):
-  return render(request, 'multi/index.html')
+  candidate_list = Candidate.objects.all()
+  question_list = Question.objects.all().order_by('number')
+  response_list = Response.objects.all().order_by('description', 'response')
+  template_name = 'multi/index.html'
+  context = {
+    'candidate_list': candidate_list,
+    'question_list': question_list,
+    'response_list': response_list,
+    }
+  return render(request, template_name, context)
+
+
+def about(request):
+    return render(request, 'about.html')    
+
+
+class CandidateDetailView(DetailView):
+  model = Candidate
+  template_name = 'multi/candidate_detail.html'
+
+  def get_context_data(self, **kwargs):
+         context = super(CandidateDetailView, self).get_context_data(**kwargs)
+         return context
+
+
